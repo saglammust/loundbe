ff972f9edfc30c7a85fd7edb09cbf9c86cb3bf2b
saglammust <saglammust@itu.edu.tr>
2021-08-16 16:25:29 +0300

    updated settings

--- loundbe/settings.py
+++ loundbe/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('LOUNDBE_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['loundbe.com','loundbe.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
