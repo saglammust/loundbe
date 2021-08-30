f324dd73265776d04221d47db29a5eeb81916467
saglammust <saglammust@itu.edu.tr>
2021-08-30 21:07:18 +0300

    fixed admin page problems

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['text','pub_date']
+    inlines = [ChoiceInline]
+    list_display = ('text', 'degree', 'pub_date')
+    list_filter = ['pub_date', 'degree']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
