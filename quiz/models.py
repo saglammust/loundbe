48a51de5e19745d7da89d15c68509290147487aa
saglammust <saglammust@itu.edu.tr>
2021-08-31 17:43:17 +0300

    change text field types

--- quiz/models.py
+++ quiz/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     def __str__(self):
         return self.text
 
-    text = models.CharField("soru metni", max_length=255)
+    text = models.TextField("soru metni", max_length=300)
     pub_date = models.DateTimeField('ekleme tarihi')
     degree = models.PositiveSmallIntegerField("zorluk derecesi",default=1)
     subject = models.CharField("konu", max_length=30, default="Genel")
@@ -20,6 +20,6 @@ class Choice(models.Model):
         return self.text
         
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="sahip soru")
-    text = models.CharField("seçenek metni", max_length=200)
+    text = models.CharField("seçenek metni", max_length=150)
     votes = models.IntegerField("oy sayısı", default=0)
     correct = models.BooleanField("seçenek doğru mu?",default=False)
