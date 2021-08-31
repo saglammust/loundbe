13f42ada1fcf451408723b46b9c13620f74faa80
saglammust <saglammust@itu.edu.tr>
2021-08-31 17:22:58 +0300

    added tag model, fixed degree

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Tag
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 4
+    extra = 0
 
+class TagInLine(admin.TabularInline):
+    model = Tag
+    extra = 0
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['text','pub_date']
-    inlines = [ChoiceInline]
+    fields = ['text','pub_date','degree']
+    inlines = [ChoiceInline, TagInLine]
     list_display = ('text', 'degree', 'pub_date')
     list_filter = ['pub_date', 'degree']
-    search_fields = ['question_text']
+    search_fields = ['text']
 
 admin.site.register(Question, QuestionAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -3,7 +3,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-
 class Question(models.Model):
     def __str__(self):
         return self.text
@@ -15,6 +14,12 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+class Tag(models.Model):
+    def __str__(self):
+        return self.text
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="sahip soru")
+    text = models.CharField("konu", max_length=30)
 
 class Choice(models.Model):
     def __str__(self):
